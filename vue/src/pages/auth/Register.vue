52050f9c3df3ae99522d4b4be18095098cb71674
Daniel <snakee0101@gmail.com>
2026-02-13 00:36:29 +0200

    started creatin register page

--- vue/src/pages/auth/Register.vue
+++ vue/src/pages/auth/Register.vue
@@ -1,8 +1,21 @@
 <script setup></script>
 
 <template>
-<div>
-    REGISTER VIEW
+<div class="auth-container">
+    <div class="auth-card">
+        <h1>Register</h1>
+
+        <form>
+            <input type="text" placeholder="Your name" required />
+            <input type="email" placeholder="Your email" required />
+            <input type="password" placeholder="Your password" required />
+
+            <div class="form-footer">
+                <button type="submit">Register</button>
+                <router-link to="/login">Click here to login</router-link>
+            </div>
+        </form>
+    </div>
 </div>
 </template>
 
